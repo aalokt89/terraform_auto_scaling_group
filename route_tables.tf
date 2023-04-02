42cd103f7686603866cc81f1a9cfe55a3aad5197
Aalok Trivedi <aaloktrivedi89@gmail.com>
2023-04-02 19:04:54 -0400

    add private subnet assoc

--- route_tables.tf
+++ route_tables.tf
@@ -1,6 +1,7 @@
 # Edit default route table for public subnets
 #----------------------------------------------------
-resource "aws_default_route_table" "public_route_table" {
+# Public rt
+resource "aws_default_route_table" "public_rt" {
   default_route_table_id = aws_vpc.vpc.default_route_table_id
 
   route {
@@ -13,11 +14,35 @@ resource "aws_default_route_table" "public_route_table" {
   }
 }
 
+# Private rt
+resource "aws_route_table" "private_rt" {
+  vpc_id = aws_vpc.vpc.id
+
+  route {
+    cidr_block        = var.all_traffic
+    natnat_gateway_id = ""
+  }
+  tags = {
+    Name      = "${var.app_name}-public-rt"
+    Terraform = "true"
+  }
+}
+
 # Create route table associations
 #----------------------------------------------------
+
+# Public rt assoc.
 resource "aws_route_table_association" "public" {
   depends_on     = [aws_subnet.public_subnets]
-  route_table_id = aws_default_route_table.public_route_table.id
+  route_table_id = aws_default_route_table.public_rt.id
   for_each       = aws_subnet.public_subnets
+  subnet_id      = each.value.id
+}
+
+# Private rt assoc.
+resource "aws_route_table_association" "public" {
+  depends_on     = [aws_subnet.private_subnets]
+  route_table_id = aws_route_table.private_rt.id
+  for_each       = aws_subnet.private_subnets
   subnet_id      = each.value.id
 }
