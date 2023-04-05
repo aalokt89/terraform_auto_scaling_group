e958de91f84d5608d232deaf859b39cc0e955c5d
Aalok Trivedi <aaloktrivedi89@gmail.com>
2023-04-04 21:47:43 -0400

    fix name

--- ec2_launch_template.tf
+++ ec2_launch_template.tf
@@ -2,7 +2,7 @@
 # EC2 web launch template
 #----------------------------------------
 resource "aws_launch_template" "web_server" {
-  name                   = "${var.app_name}-${var.web_server_name}"
+  name                   = "${var.app_name}-${var.web_server_name}-template"
   image_id               = var.web_server_ami
   instance_type          = var.web_server_type
   user_data              = filebase64("${path.module}/${var.user_data_file}")
@@ -10,6 +10,6 @@ resource "aws_launch_template" "web_server" {
   vpc_security_group_ids = [aws_security_group.ssh_sg.id, aws_security_group.web_access_sg.id]
 
   tags = {
-    "Name" = "${var.app_name}-${var.web_server_name}"
+    "Name" = "${var.app_name}-${var.web_server_name}-template"
   }
 }
