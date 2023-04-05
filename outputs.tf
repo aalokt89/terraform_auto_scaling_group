14fb2494af7b89e8504971ee3c9ea8a52a258941
Aalok Trivedi <aaloktrivedi89@gmail.com>
2023-04-05 19:16:53 -0400

    ouput subnet ids

--- outputs.tf
+++ outputs.tf
@@ -3,8 +3,33 @@ output "vpc_id" {
   value       = aws_vpc.vpc.id
 }
 
+output "public_subnets" {
+  description = "Public subnets"
+  value = [
+    for subnet in aws_subnet.private_subnets : {
+      "id" = "${subnet.id}"
+    }
+  ]
+}
+
+output "private_subnets" {
+  description = "Private subnets"
+  value = [
+    for subnet in aws_subnet.private_subnets : {
+      "id" = "${subnet.id}"
+    }
+  ]
+}
+
 output "eip" {
   description = "Elastic IP: Public"
   value       = aws_eip.eip.public_ip
 }
+
+output "alb_dns" {
+  description = "ALB public DNS"
+  value       = aws_lb.web_alb.dns_name
+}
+
+
 
