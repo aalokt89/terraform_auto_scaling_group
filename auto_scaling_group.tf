7e2381207214076fb7b70aa9f4c9650b172df21f
Aalok Trivedi <aaloktrivedi89@gmail.com>
2023-04-06 15:49:24 -0400

    add scaling policy

--- auto_scaling_group.tf
+++ auto_scaling_group.tf
@@ -5,12 +5,27 @@ resource "aws_autoscaling_group" "web_server_asg" {
   name                = "${var.app_name}-web-server-asg"
   min_size            = var.web_asg_capacity["min"]
   max_size            = var.web_asg_capacity["max"]
-  desired_capacity    = var.web_asg_capacity["desired"]
   vpc_zone_identifier = [for subnet in aws_subnet.private_subnets : subnet.id]
   target_group_arns   = [aws_lb_target_group.web_alb_tg_http.arn]
+  health_check_type   = var.web_asg_health_check_type
 
   launch_template {
     id      = aws_launch_template.web_server.id
     version = "$Latest"
+  }
+}
+
+# Scaling policy
+resource "aws_autoscaling_policy" "web_server_asg" {
+  depends_on             = [aws_autoscaling_group.web_server_asg]
+  name                   = "${var.app_name}-asg-scaling-policy"
+  autoscaling_group_name = aws_autoscaling_group.web_server_asg.name
+
+  target_tracking_configuration {
+    predefined_metric_specification {
+      predefined_metric_type = var.web_asg_scaling_policy["metric_type"]
+    }
+
+    target_value = var.web_asg_scaling_policy["target_value"]
   }
 }
