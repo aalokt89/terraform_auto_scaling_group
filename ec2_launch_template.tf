d0c85dedf8be8270c6529730c02535620e6906ec
Aalok Trivedi <aaloktrivedi89@gmail.com>
2023-04-06 12:17:36 -0400

    updated sg name

--- ec2_launch_template.tf
+++ ec2_launch_template.tf
@@ -7,7 +7,7 @@ resource "aws_launch_template" "web_server" {
   instance_type          = var.web_server_type
   user_data              = filebase64("${path.module}/${var.user_data_file}")
   key_name               = var.key_pair
-  vpc_security_group_ids = [aws_security_group.ssh_sg.id, aws_security_group.web_access_sg.id]
+  vpc_security_group_ids = [aws_security_group.ssh_sg.id, aws_security_group.alb_access_sg.id]
 
   tags = {
     "Name" = "${var.app_name}-${var.web_server_name}-template"
